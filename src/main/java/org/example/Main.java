6d9c18a8dd5decd293311a38f1946649b713191b
Asliddin <asliddinmukhtarov2004@gmail.com>
2025-02-26 01:23:21 +0500

    admin package was created and finished

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,5 @@
 package org.example;
-
-import org.example.service.adminPackage.RoomService;
-
 public class Main {
     public static void main(String[] args) {
-        while (true){
-            RoomService.roomService();
-        }
     }
 }
